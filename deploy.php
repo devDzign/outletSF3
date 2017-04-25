d882ce50d37a46edee2b5c2c4c84702b9c53cf5f
mchabour <mchabour@largus.fr>
2017-04-25 13:54:23 +0200

    yes

--- deploy.php
+++ deploy.php
@@ -4,8 +4,7 @@ require 'recipe/symfony3.php';
 
 // Configuration cool
 
-set('ssh_type', 'native');
-set('ssh_multiplexing', true);
+
 
 set('repository', 'https://github.com/devDzign/outletSF3.git');
 
@@ -17,28 +16,27 @@ add('writable_dirs', []);
 // Servers
 
 server('prod', 'ssh-deployersf.alwaysdata.net')
-    ->user('deployersf')
-    ->password('mourad__2008')
     ->set('deploy_path', '/home/deployersf/www/outletSF3')
     ->pty(true);
 
+set('keep_releases', 5);
 
 // Tasks
 
-desc('Restart PHP-FPM service');
-task(
-    'php-fpm:restart',
-    function () {
-        // The user must have rights for restart service
-        // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
-        run('sudo systemctl restart php-fpm.service');
-    }
-);
-after('deploy:symlink', 'php-fpm:restart');
-
-// [Optional] if deploy fails automatically unlock.
-after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-before('deploy:symlink', 'database:migrate');
+//desc('Restart PHP-FPM service');
+//task(
+//    'php-fpm:restart',
+//    function () {
+//        // The user must have rights for restart service
+//        // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+//        run('sudo systemctl restart php-fpm.service');
+//    }
+//);
+//after('deploy:symlink', 'php-fpm:restart');
+//
+//// [Optional] if deploy fails automatically unlock.
+//after('deploy:failed', 'deploy:unlock');
+//
+//// Migrate database before symlink new release.
+//
+//before('deploy:symlink', 'database:migrate');
